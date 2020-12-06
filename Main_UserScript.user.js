bd0ba7dc1edafbd36362da160e3941efb81f9e8c
goodguygregg <maxjac@live.se>
2020-12-06 09:36:55 +0100

    update

--- Main_UserScript.user.js
+++ Main_UserScript.user.js
@@ -9,7 +9,7 @@
 // @include      https://www.empornium.me/requests.php?*action=allcomments
 // @include      https://www.empornium.me/forum/recent*
 // @include      https://www.empornium.me/collages.php*?action=allcomments
-// @updateURL    https://github.com/goodguygregg-dev/emp-checker-ggg/raw/main/Main_UserScript.user.js
+// @downloadURL    https://github.com/goodguygregg-dev/emp-checker-ggg/raw/main/Main_UserScript.user.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
