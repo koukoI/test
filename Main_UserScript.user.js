ae9b3762595e1e7f98b74b29590b33513d112c60
goodguygregg <maxjac@live.se>
2020-12-06 09:30:23 +0100

    version

--- Main_UserScript.user.js
+++ Main_UserScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FLS Comment Checker
 // @namespace    http://tampermonkey.net/
-// @version      1.19
+// @version      1.20
 // @description  Credits to curtwagner who wrote and maintained the original script.
 // @author       goodguygregg
 // @match        https://www.empornium.me/torrents.php?*action=allcomments
