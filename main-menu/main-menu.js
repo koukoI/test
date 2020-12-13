3842f63b2a6f0447eb95e82b7333c83a19a81871
goodguygregg <maxjac@live.se>
2020-12-13 15:53:26 +0100

    same

--- main-menu/main-menu.js
+++ main-menu/main-menu.js
@@ -8,7 +8,7 @@ function addMainMenuToDom() {
     let mainMenuElement = document.createElement("div");
     mainMenuElement.innerHTML = mainMenuHtml;
     document.body.appendChild(mainMenuElement);
-
+   
     setHideAvatarLables();
     setHideSignatureLables();
 
@@ -41,7 +41,7 @@ function registerButtonsCallbacks() {
     console.log("Registering main menu buttons callbacks...");
 
 
-    console.log(collage_checker_string + " " + document.URL.indexOf(collage_checker_string));
+    
     // shows the main menu
     if((document.URL.indexOf(collage_checker_string) >= 0) || (document.URL.indexOf("https://www.empornium.me/forum/recent") >= 0) || (document.URL.indexOf("https://www.empornium.me/torrents.php") >= 0)){
         jQuery("body").on("click", "#main-menu-button", function () {
