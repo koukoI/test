f22a95e0aace4c755d5fed24f375fad6ef6047f9
goodguygregg <maxjac@live.se>
2020-12-13 15:21:20 +0100

    same

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let default_settings = {
 };
 
 // urls to determine active checker
-let collage_checker_string = "https://www.empornium.me/collages.php*";
+let collage_checker_string = "collages.php";
 
 let user_settings = GM_SuperValue.get("user_settings");
 
