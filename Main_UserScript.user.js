8f4398866b1f9d2d1f69a6a428c28eca8463b715
goodguygregg <maxjac@live.se>
2020-12-15 12:27:24 +0100

    changed urls for externals to point at dev repo

--- Main_UserScript.user.js
+++ Main_UserScript.user.js
@@ -17,23 +17,23 @@
 // @grant        GM_openInTab
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
-// @resource     mainMenuStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main-menu/main-menu-style.css
-// @resource     mainMenuHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main-menu/main-menu-html.html
-// @resource     addedPostButtonsHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/added-post-buttons/added-post-buttons-html.html
-// @resource     addedPostButtonsStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/added-post-buttons/added-post-buttons-style.css
-// @resource     progressBarHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/progress-bar/progress-bar-html.html
-// @resource     progressBarStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/progress-bar/progress-bar-style.css
-// @resource     modalHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal-html.html
-// @resource     modalSettingsHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal-settings-html.html
-// @resource     modalStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal-style.css
-// @resource     sandboxHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/sandbox/sandbox-html.html
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/gm-lib.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/sandbox/sandbox.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/progress-bar/progress-bar.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/added-post-buttons/added-post-buttons.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/post-scanner.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main-menu/main-menu.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main.js
+// @resource     mainMenuStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/main-menu/main-menu-style.css
+// @resource     mainMenuHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/main-menu/main-menu-html.html
+// @resource     addedPostButtonsHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/added-post-buttons/added-post-buttons-html.html
+// @resource     addedPostButtonsStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/added-post-buttons/added-post-buttons-style.css
+// @resource     progressBarHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/progress-bar/progress-bar-html.html
+// @resource     progressBarStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/progress-bar/progress-bar-style.css
+// @resource     modalHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/modal/modal-html.html
+// @resource     modalSettingsHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/modal/modal-settings-html.html
+// @resource     modalStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/modal/modal-style.css
+// @resource     sandboxHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/sandbox/sandbox-html.html
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/gm-lib.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/sandbox/sandbox.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/modal/modal.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/progress-bar/progress-bar.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/added-post-buttons/added-post-buttons.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/post-scanner.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/main-menu/main-menu.js
+// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/dev/main.js
 // ==/UserScript==
 // "use strict";
