68a0db46d10b0697b71a1b6314c441a638290fd4
koukol <webjunkregs@gmail.com>
2020-12-27 21:35:29 +0100

    test

--- Main_UserScript.user.js
+++ Main_UserScript.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         FLS Comment Checker Goodies
+// @name         Checker Goodies
 // @namespace    http://tampermonkey.net/
 // @version      1.64
 // @description  Credits to curtwagner who wrote and maintained the original script.
-// @author       goodguygregg
+// @author       koukol
 // @match        https://www.empornium.me/torrents.php?*action=allcomments
 // @include      https://www.empornium.me/torrents.php?*action=allcomments
 // @include      https://www.empornium.me/requests.php?*action=allcomments
@@ -16,23 +16,23 @@
 // @grant        GM_openInTab
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
-// @resource     mainMenuStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main-menu/main-menu-style.css
-// @resource     mainMenuHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main-menu/main-menu-html.html
-// @resource     addedPostButtonsHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/added-post-buttons/added-post-buttons-html.html
-// @resource     addedPostButtonsStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/added-post-buttons/added-post-buttons-style.css
-// @resource     progressBarHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/progress-bar/progress-bar-html.html
-// @resource     progressBarStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/progress-bar/progress-bar-style.css
-// @resource     modalHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal-html.html
-// @resource     modalSettingsHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal-settings-html.html
-// @resource     modalStyle https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal-style.css
-// @resource     sandboxHtml https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/sandbox/sandbox-html.html
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/gm-lib.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/sandbox/sandbox.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/modal/modal.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/progress-bar/progress-bar.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/added-post-buttons/added-post-buttons.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/post-scanner.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main-menu/main-menu.js
-// @require      https://raw.githubusercontent.com/goodguygregg-dev/emp-checker-ggg/main/main.js
+// @resource     mainMenuStyle https://raw.githubusercontent.com/koukoI/test/goodies/main-menu/main-menu-style.css
+// @resource     mainMenuHtml https://raw.githubusercontent.com/koukoI/test/goodies/main-menu/main-menu-html.html
+// @resource     addedPostButtonsHtml https://raw.githubusercontent.comkoukoI/test/goodies/added-post-buttons/added-post-buttons-html.html
+// @resource     addedPostButtonsStyle https://raw.githubusercontent.com/koukoI/test/goodies/added-post-buttons/added-post-buttons-style.css
+// @resource     progressBarHtml https://raw.githubusercontent.com/koukoI/test/goodies/progress-bar/progress-bar-html.html
+// @resource     progressBarStyle https://raw.githubusercontent.com/koukoI/test/goodies/progress-bar/progress-bar-style.css
+// @resource     modalHtml https://raw.githubusercontent.com/koukoI/test/goodies/modal/modal-html.html
+// @resource     modalSettingsHtml https://raw.githubusercontent.com/koukoI/test/goodies/modal/modal-settings-html.html
+// @resource     modalStyle https://raw.githubusercontent.com/koukoI/test/goodies/modal/modal-style.css
+// @resource     sandboxHtml https://raw.githubusercontent.com/koukoI/test/goodies/sandbox/sandbox-html.html
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/gm-lib.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/sandbox/sandbox.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/modal/modal.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/progress-bar/progress-bar.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/added-post-buttons/added-post-buttons.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/post-scanner.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/main-menu/main-menu.js
+// @require      https://raw.githubusercontent.com/koukoI/test/goodies/main.js
 // ==/UserScript==
 // "use strict";
